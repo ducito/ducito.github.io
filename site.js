f64ec3f65a1e6d848ed847928a0e9280ca635caa
Nguyen Ngoc Duc <duc.nguyenngoc@axonactive.com>
2017-09-10 20:58:27 +0700

    change redirect page

--- site.js
+++ site.js
@@ -1,12 +1,14 @@
 
-var c = 10;
+var c = 5;
 var t;
+var timeElemet = document.getElementById("time-txt");
 
 function timedCount() {
-    document.getElementById("time-txt").value = c;
+    // timeElemet.value = c;
+    // console.log(c, timeElemet);
     c -= 1;
     t = setTimeout(function(){ timedCount() }, 1000);
     if( c == 0) {
-        window.location.assign("http://nnduc.github.io/?utm_campaign=redirect&utm_medium=email&utm_source=newsletter&utm_content=toplink")
+        window.location.assign("http://nnduc.me/?utm_campaign=redirect&utm_medium=email&utm_source=newsletter&utm_content=toplink")
     }
 }
